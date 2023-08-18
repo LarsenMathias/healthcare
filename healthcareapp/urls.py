1c2aabd465c176e065bc5131a34fd46c5e9f9b15
LarsenMathias <larsenm2020@gmail.com>
2023-08-18 20:02:03 +0530

    coommit

--- healthcareapp/urls.py
+++ healthcareapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
       path('dashboard/', views.dashboard, name='dashboard'),
          path('logout/', views.logout, name='logout'),
